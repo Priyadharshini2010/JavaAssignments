d62f10131c0389c0739d27c0095a41cd7e61750e
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-09-28 18:35:30 +0530

    init commit

--- src/com/assignment3/MultilevelInheritance.java
+++ src/com/assignment3/MultilevelInheritance.java
@@ -1,4 +1,27 @@
 package com.assignment3;
 
+class Display           //GP Class
+{
+    static int y;       //Static Variable
+    int x;              //Instance Variable
+
+    static void disp2()     //Static Method
+    {
+        Display d1 = new Display();
+        int z = 30;         //Local Variable
+        d1.x = 10;
+        y = 20;
+        System.out.println("Grand-Parent Class Instance Method");
+    }
+
+    void disp1()        //Instance Method
+    {
+        int z = 30;         //Local Variable
+        x = 10;
+        y = 20;
+        System.out.println("Grand-Parent Class Instance Method");
+    }
+}
+
 public class MultilevelInheritance {
 }
