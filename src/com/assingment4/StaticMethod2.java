36057a255f1371f18358d8b60abf9955e29954b7
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-09-30 18:22:57 +0530

    Accessing instance method from static  method within a class

---
+++ src/com/assingment4/StaticMethod2.java
@@ -0,0 +1,25 @@
+package com.assingment4;
+
+public class StaticMethod2 {
+    static void employee2() {
+        StaticMethod2 sM1 = new StaticMethod2();
+        sM1.employee1();
+        System.out.println("Static Method");
+        float salary = 200000;
+        int id = 2;
+        String name = "BBB";
+        System.out.println("Emp ID: " + id + " " + ",Name: " + name + " " + ",Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        employee2();
+    }
+
+    void employee1() {
+        System.out.println("Instance Method");
+        float salary = 100000;
+        int id = 1;
+        String name = "AAA";
+        System.out.println("Emp ID: " + id + " " + ",Name: " + name + " " + ",Salary: " + salary);
+    }
+}
