411fd9cdf533e418ebeb6ef8285a252aa289c939
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-09-30 16:58:46 +0530

    init commit

---
+++ src/com/assingment4/StaticMethod1.java
@@ -0,0 +1,22 @@
+package com.assingment4;
+
+public class StaticMethod1 {
+    static int a;
+
+    static void method1() {
+        a = 30;
+        System.out.println("Method 1 " + a);
+    }
+
+    static void method2() {
+        method1();
+        a = 40;
+        System.out.println("Method 2 " + a);
+    }
+
+    public static void main(String[] args) {
+        StaticMethod1.method2();
+    }
+
+
+}
