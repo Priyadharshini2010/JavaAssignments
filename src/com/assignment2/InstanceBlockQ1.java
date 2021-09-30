b71a2e55c45bf488c389bb3bfac73c31ae61b374
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-09-30 21:16:28 +0530

    Accessing variables and method using Instance Block

--- src/com/assignment2/InstanceBlockQ1.java
+++ src/com/assignment2/InstanceBlockQ1.java
@@ -1,36 +1,35 @@
 package com.assignment2;
 
 public class InstanceBlockQ1 {
-    static float salary;    //Static Variable
-    String name;            //Instance Variable
+    static float salary = 100000;    //Static Variable
+    String name = "AAA";            //Instance Variable
 
     {                                       //Instance Block
         System.out.println("Instance Block");
         int id = 12;
-        name = "AAA";
-        salary = 100000;
         System.out.println("EMPLOYEE ID = " + id);
+        System.out.println("EMPLOYEE NAME = " + name);
+        System.out.println("EMPLOYEE SALARY = " + salary);
+        empl();
+        employee();
     }
 
     InstanceBlockQ1() {                      //Constructor
+
         System.out.println("Constructor");
     }
 
     static void empl() {                     //Static Method
         System.out.println("Static Method");
-        System.out.println("SALARY = " + salary);
+    }
+
+    void employee() {                   //Instance Method
+        System.out.println("Instance Method");
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome");
         InstanceBlockQ1 iBq1 = new InstanceBlockQ1();
-        iBq1.employee();
-        InstanceBlockQ1.empl();
-    }
-
-    void employee() {                   //Instance Method
-        System.out.println("Instance Method");
-        System.out.println("NAME = " + name);
     }
 }
 
