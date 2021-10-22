7fec56522d89e52fa8a1f8203dfb67830e23c681
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-10-22 20:37:06 +0530

    Do While

---
+++ src/com/assignment5/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.assignment5;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println(a);
+            a++;
+        }
+        while (a <= 10);
+    }
+}
