f00a1633083fc7ce29c583a1d3e66cf66ae6cb6c
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-10-22 18:39:58 +0530

    While Loop

---
+++ src/com/assignment5/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.assignment5;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int a = 10;
+        while (a != 0) {
+            System.out.println(a);
+            a--;
+        }
+    }
+}
