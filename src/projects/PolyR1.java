ae6fe9d0b444836ed02db55cd1ec750e1e2691eb
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-02 17:20:05 +0530

    Polymorphism : Overriding R1.

---
+++ src/projects/PolyR1.java
@@ -0,0 +1,18 @@
+package projects;
+
+class PolyA {
+    void method1() {
+        System.out.println("Parent Overridden");
+    }
+}
+
+public class PolyR1 extends PolyA {
+    public static void main(String[] args) {
+        new PolyR1().method1();
+
+    }
+
+    void method1() {
+        System.out.println("Child Class Overriding");
+    }
+}
