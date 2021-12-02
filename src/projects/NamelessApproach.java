cb37df926439b553cf27eb9311a0fa0b30e1d60d
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-02 10:47:08 +0530

    Obj Creation : Named Approach

---
+++ src/projects/NamelessApproach.java
@@ -0,0 +1,23 @@
+package projects;
+
+public class NamelessApproach {
+    NamelessApproach() {
+        System.out.println("NLA 0-arg Const");
+    }
+
+    NamelessApproach(int a) {
+        System.out.println("NLA 1-arg Const");
+    }
+
+    NamelessApproach(int a, int b) {
+        System.out.println("NLA 2-arg Const");
+    }
+
+    public static void main(String[] args) {
+        new NamelessApproach();
+        new NamelessApproach(1);
+        new NamelessApproach(1, 2);
+
+
+    }
+}
