1c1ba0e79f61c0277b589edcc1b9e15fef23c22a
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-03 13:15:16 +0530

    Variables

---
+++ src/variables/Variables.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Variables {
+    static int b = 20; //Static Variable
+    int a = 10;//Instance Variable
+
+    public static void main(String[] args) {
+        int c = 30;//Local variable
+        Variables v1 = new Variables();
+        System.out.println("Instance Variable " + v1.a);
+        System.out.println("Static Variable " + v1.b);//Obj Access
+        System.out.println("Static Variable " + b);// Direct Access
+        System.out.println("Static Variable " + Variables.b); //Class name
+        System.out.println("Local Variable " + c);
+    }
+}
