0efe7521f7f12b446f56bb4794cfe1c83d711d39
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-03 17:39:38 +0530

    Methods

--- src/exercises/MethodSamples.java
+++ src/exercises/MethodSamples.java
@@ -5,18 +5,25 @@ public class MethodSamples {
         MethodSamples ms1 = new MethodSamples();
         short dispNum = ms1.display((short) 10000);
         System.out.println("Display number(short) = " + dispNum);
+
         byte showNumber = ms1.displayNumber((byte) 20);
         System.out.println("Display number(byte) = " + showNumber);
+
         boolean verify = ms1.verifyTrue(true);
         System.out.println("Verify true or false = " + verify);
+
         char alphabet = ms1.displayAlphabet('a');
         System.out.println("Concatenation of 2 Alphabets = " + alphabet);
+
         int intResult = ms1.add(10, 20);
         System.out.println("Addition Result = " + intResult);
+
         float floatResult = ms1.multiply(10, intResult);
         System.out.println("Multiplication Result = " + floatResult);
+
         double markResult = ms1.multiplyTwoNum(686, floatResult);
         System.out.println("Multiplication of two numbers = " + markResult);
+
         long longResult = ms1.addition(1000000960900000L, 2330074565000000L);
         System.out.println("Addition of two double value = " + longResult);
 
