682ef3ad208c2e28898fd3f662b05184e9d60ae5
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-03 14:34:04 +0530

    Boolean

---
+++ src/datatypes/BooleanType.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class BooleanType {
+    public static void main(String[] args) {
+        boolean a = true;
+        if (a == true) {
+            System.out.println("Hello World");
+        }
+        boolean b = false;
+        System.out.println(b);
+    }
+}
