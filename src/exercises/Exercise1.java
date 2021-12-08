6477a8b03140fdaee865a38988b1c76e67e06cf1
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-08 12:49:27 +0530

    Exercise

--- src/exercises/Exercise1.java
+++ src/exercises/Exercise1.java
@@ -1,23 +1,47 @@
 package exercises;
 
 class RBIBank {
-    int oldInterest = 6;
+
+    int diff;
+    int oldInterest = 2;
 
     int interest(int interestRate) {
-        return interestRate - oldInterest;
+        diff = interestRate - oldInterest;
+        return diff;
     }
 }
 
+class SBIBank extends RBIBank {
+    int oldInterest = 7;
+
+    int interest(int interestRate) {
+        return oldInterest + interestRate;
+    }
+}
+
+class HDFCBank extends RBIBank {
+    int oldInterest = 4;
+
+    int interest(int interestRate) {
+        return oldInterest + interestRate;
+    }
+}
 
 public class Exercise1 extends RBIBank {
-    int oldInterest = 9;
+    int oldInterest = 5;
 
     public static void main(String[] args) {
         Exercise1 ex1 = new Exercise1();
         RBIBank rbi = new RBIBank();
+        SBIBank sbi = new SBIBank();
+        HDFCBank hdfc = new HDFCBank();
         int rbiDiff = rbi.interest(9);
-        int finalInterest = ex1.interest(rbiDiff);
-        System.out.println(finalInterest);
+        int iciciInterest = ex1.interest(rbiDiff);
+        int sbiInterest = sbi.interest(rbiDiff);
+        int hdfcInterest = hdfc.interest(rbiDiff);
+        System.out.println(iciciInterest);
+        System.out.println(sbiInterest);
+        System.out.println(hdfcInterest);
 
     }
 
