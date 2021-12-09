7955b3fe6f569d3c8883e874a593b05be96fe128
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-09 12:22:33 +0530

    If Statement

---
+++ src/exercises/flowcontrol/IfStatement.java
@@ -0,0 +1,10 @@
+package exercises.flowcontrol;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        int number = 100;
+        if (number > 99 && number < 999) {
+            System.out.println(number);
+        }
+    }
+}
