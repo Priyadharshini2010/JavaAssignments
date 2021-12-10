8c348ba00119df9e308c56daa654d48ed0cd5a6f
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-10 17:06:55 +0530

    Access Modifiers

--- src/exercises/accessmodifiers/access1/NestedClass.java
+++ src/exercises/accessmodifiers/access1/NestedClass.java
@@ -15,9 +15,14 @@ class OuterClass {
             System.out.println("Candidate Age = " + age);
         }
 
-        protected void confirmVote() {
+        private void helpMessage() {
             canVote();
             System.out.println("Can cast vote");
+        }
+
+        protected void confirmVote() {
+            helpMessage();
+            System.out.println("Day : Saturday");
         }
 
         void displayResult() {
@@ -31,12 +36,24 @@ class OuterClass {
         }
 
     }
+
+    class Message extends InnerClass {
+        private void message() {
+            showResult();
+            System.out.println("SOS");
+        }
+
+        void sos() {
+            message();
+            System.out.println("Contact for help");
+        }
+    }
 }
 
 public class NestedClass extends OuterClass {
     public static void main(String[] args) {
         OuterClass oclass = new OuterClass();
-        OuterClass.InnerClass innerObject = oclass.new InnerClass();
-        innerObject.showResult();
+        OuterClass.Message msg = oclass.new Message();
+        msg.sos();
     }
 }
