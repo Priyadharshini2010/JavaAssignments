6f0522015cf7a7331c35d8966c9fe2bc154b0220
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-13 16:24:33 +0530

    Updated

--- src/exercises/loopexamples/WhileEx.java
+++ src/exercises/loopexamples/WhileEx.java
@@ -2,10 +2,12 @@ package exercises.loopexamples;
 
 public class WhileEx {
     public static void main(String[] args) {
-        int i = 5;
+        int i = 1;
+        int sum = 0;
         while (i <= 10) {
-            System.out.println(i);
+            sum = sum + i;
             i++;
         }
+        System.out.println(sum);
     }
 }
