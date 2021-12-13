cd5f61f42691da6c1f562658ad176b55e5edabd1
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-13 14:06:55 +0530

    Updated

--- src/exercises/loopexamples/PyramidFor.java
+++ src/exercises/loopexamples/PyramidFor.java
@@ -2,8 +2,8 @@ package exercises.loopexamples;
 
 public class PyramidFor {
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; ++i) {
-            for (int j = 1; j <= i; ++j) {
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.println(j);
             }
             System.out.println();
