1249945072b028204cfd6ea11de9173e972e6fe1
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-13 14:05:05 +0530

    Updated

---
+++ src/exercises/loopexamples/ForEachLoop.java
@@ -0,0 +1,10 @@
+package exercises.loopexamples;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
