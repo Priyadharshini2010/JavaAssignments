73520f32fd3c6be2d62d76bbfa02be3060ffc6a2
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-13 14:17:04 +0530

    Updated

--- src/exercises/loopexamples/PyramidFor.java
+++ src/exercises/loopexamples/PyramidFor.java
@@ -4,7 +4,7 @@ public class PyramidFor {
     public static void main(String[] args) {
         for (int i = 1; i <= 3; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.println(j);
+                System.out.println("* ");
             }
             System.out.println();
         }
---
+++ src/exercises/loopexamples/WhileEx.java
@@ -0,0 +1,11 @@
+package exercises.loopexamples;
+
+public class WhileEx {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
