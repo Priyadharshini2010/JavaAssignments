e7a588654f10bf7572f7f30a5bbcea3a77592133
Priya2010Kutty <gkpriyadharsini@gmail.com>
2021-12-14 18:07:45 +0530

    Updated

--- src/exercises/loopexamples/ReverseNumber.java
+++ src/exercises/loopexamples/ReverseNumber.java
@@ -2,13 +2,13 @@ package exercises.loopexamples;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        int num = 2986, reversedNum = 0;
+        int num = 2986, reverseNum = 0;
         while (num != 0) {
             int moduloNum = num % 10;
-            reversedNum = reversedNum * 10 + moduloNum;
+            reverseNum = reverseNum * 10 + moduloNum;
             num /= 10;
         }
 
-        System.out.println("Reversed Number: " + reversedNum);
+        System.out.println("Reversed Number: " + reverseNum);
     }
 }
