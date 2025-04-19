3bbbb417b6052e06b9a4a2b18aa98a654c82a33f
Priyadharshini K <91552587+Priyadharshini2010@users.noreply.github.com>
2025-04-19 21:05:42 +0530

    Update WrapperClass.java

--- src/exercises/variables/WrapperClass.java
+++ src/exercises/variables/WrapperClass.java
@@ -7,16 +7,17 @@ public class WrapperClass {
         wc1.employee2();
 
     }
-
+//Object -- Integer,Character
     void employee1() {
         Integer empId = null;
         Character empTeam = 'A';
-        Float salary = 50000f;
+        Float salary = 50000f; // for float f should be given as suffix else compliation error would occur.
         String empName = "aaaa";
+        Double emppremium = 20083.9898; // d can be given as suffix, even if not given no issues
         System.out.println("Employee Id : " + empId + " " + "Employee Name : " + empName);
         System.out.println("Employee Team : " + empTeam + " " + "Salary : " + salary);
     }
-
+//Datatypes -- int,char
     void employee2() {
         int empId = 0;
         if (empId == 0) {
